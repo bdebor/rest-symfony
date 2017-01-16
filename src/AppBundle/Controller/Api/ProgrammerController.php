5ce280ac828ea49a2b72a1faf5132ae7d2865c36
Benoit Debordeaux <b.debordeaux2@gmail.com>
2017-01-16 14:38:58 +0100

    2. Finish POST with a Form

--- src/AppBundle/Controller/Api/ProgrammerController.php
+++ src/AppBundle/Controller/Api/ProgrammerController.php
@@ -12,13 +12,22 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use AppBundle\Controller\BaseController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class ProgrammerController extends Controller
+class ProgrammerController extends BaseController
 {
     /**
      * @Route("/api/programmers")
      */
-    public function newAction()
-    {
-		return new Response('Let\'s do this!');
-    }
+	public function newAction(Request $request)
+	{
+		$data = json_decode($request->getContent(), true);
+		$programmer = new Programmer($data['nickname'], $data['avatarNumber']);
+		$programmer->setTagLine($data['tagLine']);
+		$programmer->setUser($this->findUserByUsername('weaverryan'));
+
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($programmer);
+		$em->flush();
+
+		return new Response('It worked. Believe me - I\'m an API');
+	}
 }
