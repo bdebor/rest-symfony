e4a4e46f19741d0b6d1d9f5dbfdcb10dd72e59f3
Benoit Debordeaux <b.debordeaux2@gmail.com>
2017-02-14 16:27:33 +0100

    13. The Helpful Detail Key

--- src/AppBundle/EventListener/ApiExceptionSubscriber.php
+++ src/AppBundle/EventListener/ApiExceptionSubscriber.php
@@ -29,6 +29,10 @@ class ApiExceptionSubscriber implements EventSubscriberInterface
             $apiProblem = new ApiProblem(
                 $statusCode
             );
+
+            if ($e instanceof HttpExceptionInterface) {
+                $apiProblem->set('detail', $e->getMessage());
+            }
         }
 
         $response = new JsonResponse(
