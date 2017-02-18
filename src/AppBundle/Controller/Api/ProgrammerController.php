afb99635191cac7a24d25bc26c9775a557cf34e7
Benoit Debordeaux <b.debordeaux2@gmail.com>
2017-02-18 09:46:12 +0100

    7. Adding Battle Validation

--- src/AppBundle/Controller/Api/ProgrammerController.php
+++ src/AppBundle/Controller/Api/ProgrammerController.php
@@ -151,33 +151,4 @@ class ProgrammerController extends BaseController
 
 		return new Response(null, 204);
 	}
-
-	private function getErrorsFromForm(FormInterface $form)
-	{
-		$errors = array();
-		foreach ($form->getErrors() as $error) {
-			$errors[] = $error->getMessage();
-		}
-		foreach ($form->all() as $childForm) {
-			if ($childForm instanceof FormInterface) {
-				if ($childErrors = $this->getErrorsFromForm($childForm)) {
-					$errors[$childForm->getName()] = $childErrors;
-				}
-			}
-		}
-		return $errors;
-	}
-
-	private function throwApiProblemValidationException(FormInterface $form){
-		$errors = $this->getErrorsFromForm($form);
-
-		$apiProblem = new ApiProblem(
-			400,
-			ApiProblem::TYPE_VALIDATION_ERROR
-		);
-
-		$apiProblem->set('errors', $errors);
-
-		throw new ApiProblemException($apiProblem);
-	}
 }
